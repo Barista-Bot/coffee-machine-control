632e80bdaa4fef8c8fcf2b80b17de627bd9fe965
Rob Bishop <quantum.rob@gmail.com>
2013-11-15 13:16:43 +0000

    Update coffee_machine_server.py
    - Motors now switched off when not in use
    - Added control of loader motors
    - Capsule dispenser now rotates to correct position

--- scripts/coffee_machine_server.py
+++ scripts/coffee_machine_server.py
@@ -4,15 +4,21 @@ from coffee_machine_control.srv import *
 import rospy, time
 import RPi.GPIO as GPIO
 
-# capsule type: (loader position, capsule count)
-coffee_capsule_loader={
+# capsule type: (dispenser position, capsule count)
+coffee_capsule_dispenser={
     'coffee_type: mocha':(1,10),
     'coffee_type: caramel':(2,10),
     'coffee_type: vanilla':(3,10),
     'coffee_type: espresso':(4,10)
 }
 
-output_bus = [17, 22, 23, 4]
+current_coffee_capsule_dispenser_position = 1
+steps_per_quarter_rotation = #TBC
+loader_motor_activation_time = #TBC
+
+stepper_output_gpio_bus = [17, 22, 23, 4]
+motor_left_gpio = [] #TBC
+motor_right_gpio = [] #TBC
 
 step_sequence = [
     [1, 0, 0, 0],
@@ -27,38 +33,59 @@ step_sequence = [
 
 def setup_gpio():
     GPIO.setmode(GPIO.BCM)
-    for gpio_channel in output_bus:
+    for gpio_channel in stepper_output_gpio_bus:
         GPIO.setup(gpio_channel, GPIO.OUT)
-        GPIO.output(gpio_channel, False)
+        GPIO.output(gpio_channel, True) # all outputs are inverted
+    GPIO.setup(motor_left_gpio, GPIO.OUT)
+    GPIO.setup(motor_right_gpio, GPIO.OUT)
+    GPIO.output(motor_left_gpio, True)
+    GPIO.output(motor_right_gpio, True)
+    
+def setup_coffee_for_manual_vending(coffee_type):
 
-def setup_coffee_for_vending(coffee_type):
-
-    print str(coffee_type)+" selected."
+    print "Coffee Machine: "+str(coffee_type)+" selected."
 
     if (coffee_capsule_loader[str(coffee_type)][1] == 0):
         return coffee_machineResponse(False, "Out of chosen capsules")
 
-
-    # Rotate capsule holder
-
-    # Load capsule
-    print "Loading capsule"
-    while True:
-    	for step_count in range(0, 8):
-        	print "."
-        	for bit in range(0, 4):
-            		if (step_sequence[step_count][bit] == 0):
-                		GPIO.output(output_bus[bit], True)
-            		else:
-                		GPIO.output(output_bus[bit], False)
-		time.sleep(0.004)
-    print "Capsule loading complete, ready to vend."
-    return coffee_machineResponse(True, "Capsule loaded.")
+    # Rotate capsule holder to select chosen capsule
+    print "Coffee Machine: Rotating capsule dispenser to position"+coffee_capsule_loader[str(coffee_type)][0]
+    quarter_rotation_steps_needed = (coffee_capsule_loader[str(coffee_type)][0] - current_coffee_capsule_dispenser_position) % 4
+    for quarter_rotation_count in range(0, quarter_rotation_steps_needed)    
+	    for step_count in range(1, steps_per_quarter_rotation)
+    		for step in range(0, 8):
+               		for gpio_channel in range(0, 4):
+            			if (step_sequence[step][gpio_channel] == 0):
+                			GPIO.output(stepper_gpio_output_bus[gpio_channel], True)
+            			else:
+                			GPIO.output(stepper_gpio_output_bus[gpio_channel], False)
+			time.sleep(0.004)
+			
+    # Disable stepper to prevent overheating
+    for gpio_channel in stepper_output_gpio_bus:
+        GPIO.setup(gpio_channel, GPIO.OUT)
+        GPIO.output(gpio_channel, True) 
+        
+    print "Coffee Machine: Capsule selected and added to loader"
+ 
+    # Load selected capsule into machine
+    print "Coffee Machine: Loading capsule into Nespresso machine"
+    GPIO.output(motor_left_gpio, False)
+    GPIO.output(motor_right_gpio, False)
+    time.sleep(loader_motor_activation_time)
+    GPIO.output(motor_left_gpio, True)
+    GPIO.output(motor_right_gpio, True)
+    print "Coffee Machine: Capsule loaded into Nespresso machine, ready for manual coffee vend"
+ 
+    # Decrement capsule count
+    coffee_capsule_loader[str(coffee_type)][1] -= 1
+    
+    return coffee_machineResponse(True, "Ready")
 
 def coffee_machine_control_server():
     setup_gpio()
     rospy.init_node('coffee_machine_control_server')
-    rospy.Service('coffee_machine', coffee_machine, setup_coffee_for_vending)
+    rospy.Service('coffee_machine', coffee_machine, setup_coffee_for_manual_vending)
     print "Ready to load capsules"
     rospy.spin()
 
