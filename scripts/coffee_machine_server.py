d4e08c7527b02569c1aaa7126cfe3e5679c13f31
root <root@Sasha.(none)>
2013-11-18 12:16:55 +0000

    Further modifications V0.22

--- scripts/coffee_machine_server.py
+++ scripts/coffee_machine_server.py
@@ -3,6 +3,7 @@
 from coffee_machine_control.srv import *
 import rospy, time
 import RPi.GPIO as GPIO
+import pickle
 
 # capsule type: (dispenser position, capsule count)
 coffee_capsule_dispenser={
@@ -12,13 +13,13 @@ coffee_capsule_dispenser={
     'coffee_type: espresso':(4,10)
 }
 
-current_coffee_capsule_dispenser_position = 1
-steps_per_quarter_rotation = #TBC
-loader_motor_activation_time = #TBC
+current_coffee_capsule_dispenser_position = pickle.load(open("/home/pi/settings.p", 'rb')) 
+steps_per_quarter_rotation = 128
+loader_motor_activation_time = 2
 
 stepper_output_gpio_bus = [17, 22, 23, 4]
-motor_left_gpio = [] #TBC
-motor_right_gpio = [] #TBC
+motor_left_gpio = 24
+motor_right_gpio = 25
 
 step_sequence = [
     [1, 0, 0, 0],
@@ -45,21 +46,23 @@ def setup_coffee_for_manual_vending(coffee_type):
 
     print "Coffee Machine: "+str(coffee_type)+" selected."
 
-    if (coffee_capsule_loader[str(coffee_type)][1] == 0):
+    if (coffee_capsule_dispenser[str(coffee_type)][1] == 0):
         return coffee_machineResponse(False, "Out of chosen capsules")
 
     # Rotate capsule holder to select chosen capsule
-    print "Coffee Machine: Rotating capsule dispenser to position"+coffee_capsule_loader[str(coffee_type)][0]
-    quarter_rotation_steps_needed = (coffee_capsule_loader[str(coffee_type)][0] - current_coffee_capsule_dispenser_position) % 4
-    for quarter_rotation_count in range(0, quarter_rotation_steps_needed)    
-	    for step_count in range(1, steps_per_quarter_rotation)
+    print "Coffee Machine: Current capsule dispenser position is "+str(current_coffee_capsule_dispenser_position)
+    print "Coffee Machine: Rotating capsule dispenser to position "+str(coffee_capsule_dispenser[str(coffee_type)][0])
+#    pickle.dump(coffee_capsule_dispenser[str(coffee_type)][0], open("/home/pi/settings.p", "wb"))
+    quarter_rotation_steps_needed = (coffee_capsule_dispenser[str(coffee_type)][0] - current_coffee_capsule_dispenser_position) % 4
+    for quarter_rotation_count in range(0, quarter_rotation_steps_needed):    
+	for step_count in range(1, steps_per_quarter_rotation):
     		for step in range(0, 8):
                		for gpio_channel in range(0, 4):
             			if (step_sequence[step][gpio_channel] == 0):
-                			GPIO.output(stepper_gpio_output_bus[gpio_channel], True)
+                			GPIO.output(stepper_output_gpio_bus[gpio_channel], True)
             			else:
-                			GPIO.output(stepper_gpio_output_bus[gpio_channel], False)
-			time.sleep(0.004)
+                			GPIO.output(stepper_output_gpio_bus[gpio_channel], False)
+			time.sleep(0.01)
 			
     # Disable stepper to prevent overheating
     for gpio_channel in stepper_output_gpio_bus:
@@ -78,8 +81,9 @@ def setup_coffee_for_manual_vending(coffee_type):
     print "Coffee Machine: Capsule loaded into Nespresso machine, ready for manual coffee vend"
  
     # Decrement capsule count
-    coffee_capsule_loader[str(coffee_type)][1] -= 1
-    
+    coffee_capsule_dispenser[str(coffee_type)] = (coffee_capsule_dispenser[str(coffee_type)][0],  coffee_capsule_dispenser[str(coffee_type)][1] - 1)
+    print "Coffee Machine: "+str(coffee_capsule_dispenser[str(coffee_type)][1])+" of "+str(coffee_type)+" remaining"    
+  
     return coffee_machineResponse(True, "Ready")
 
 def coffee_machine_control_server():
