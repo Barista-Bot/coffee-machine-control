284cd1f1f22fbefebebc2cbb94ab009b32261be4
Human <human@prl-2.(none)>
2013-11-28 18:05:39 +0000

    Updated service (or something)

--- scripts/coffee_machine_server.py
+++ scripts/coffee_machine_server.py
@@ -15,12 +15,17 @@ coffee_capsule_dispenser={
 
 current_coffee_capsule_dispenser_position = pickle.load(open("/home/pi/settings.p", 'rb')) 
 steps_per_quarter_rotation = 128
-loader_motor_reverse_activation_time = 2
-loader_motor_forward_activation_time = 2
+loader_motor_reverse_activation_time = 3
+loader_motor_forward_activation_time = 1
+loader_motor_partialforward_activation_time = 1
+
+freq = 230
+period = 1/freq
 
 stepper_output_gpio_bus = [17, 22, 23, 4]
 motor_reverse_gpio = 24
 motor_forward_gpio = 25
+coffee_switch_gpio = 27
 
 step_sequence = [
     [1, 0, 0, 0],
@@ -37,23 +42,20 @@ def setup_gpio():
     GPIO.setmode(GPIO.BCM)
     for gpio_channel in stepper_output_gpio_bus:
         GPIO.setup(gpio_channel, GPIO.OUT)
-        GPIO.output(gpio_channel, False)
+        GPIO.output(gpio_channel, False) 
     GPIO.setup(motor_forward_gpio, GPIO.OUT)
     GPIO.setup(motor_reverse_gpio, GPIO.OUT)
     GPIO.output(motor_forward_gpio, False)
     GPIO.output(motor_reverse_gpio, False)
-    
+    GPIO.setup(coffee_switch_gpio, GPIO.OUT)
+    GPIO.output(coffee_switch_gpio, False)
+
 def setup_coffee_for_manual_vending(coffee_type):
 
     print "Coffee Machine: "+str(coffee_type)+" selected."
 
     if (coffee_capsule_dispenser[str(coffee_type)][1] == 0):
         return coffee_machineResponse(False, "Out of chosen capsules")
-
-    # Empty loader of previous cartridge
-    GPIO.output(motor_forward_gpio, True)
-    time.sleep(loader_motor_forward_activation_time)
-    GPIO.output(motor_forward_gpio, False)
 
     # Rotate capsule holder to select chosen capsule
     print "Coffee Machine: Current capsule dispenser position is "+str(current_coffee_capsule_dispenser_position)
@@ -65,17 +67,32 @@ def setup_coffee_for_manual_vending(coffee_type):
     		for step in range(0, 8):
                		for gpio_channel in range(0, 4):
             			if (step_sequence[step][gpio_channel] == 0):
-                			GPIO.output(stepper_output_gpio_bus[gpio_channel], False
+                			GPIO.output(stepper_output_gpio_bus[gpio_channel], False)
             			else:
                 			GPIO.output(stepper_output_gpio_bus[gpio_channel], True)
 			time.sleep(0.01)
 			
+    # Open capsule tray to allow capsule to be loaded
+    GPIO.output(motor_forward_gpio, True)
+    time.sleep(loader_motor_partialforward_activation_time)
+    GPIO.output(motor_forward_gpio, False) 
+
+    # Pause to let capsule drop
+    time.sleep(1)
+
+    # Complete capsule opening
+    GPIO.output(motor_forward_gpio, True)
+    time.sleep(loader_motor_forward_activation_time)
+    GPIO.output(motor_forward_gpio, False)
+
     # Disable stepper to prevent overheating
     for gpio_channel in stepper_output_gpio_bus:
         GPIO.setup(gpio_channel, GPIO.OUT)
         GPIO.output(gpio_channel, False) 
         
     print "Coffee Machine: Capsule selected and added to loader"
+
+    time.sleep(1)
  
     # Load selected capsule into machine
     print "Coffee Machine: Loading capsule into Nespresso machine"
@@ -83,8 +100,23 @@ def setup_coffee_for_manual_vending(coffee_type):
     time.sleep(loader_motor_reverse_activation_time)
     GPIO.output(motor_reverse_gpio, False)
     
-    print "Coffee Machine: Capsule loaded into Nespresso machine, ready for manual coffee vend"
+    print "Coffee Machine: Capsule loaded into Nespresso machine, ready for coffee vend"
  
+    # Dispense coffee
+
+    #Push Button
+    for count in range(0, 100):
+	GPIO.output(27, 1)
+	time.sleep(0.0020)
+	GPIO.output(27, 0)
+	time.sleep(0.0020)
+    #Retract ServoMotor
+    for count in range(0, 100):
+	GPIO.output(27, 1)
+	time.sleep(0.003)
+	GPIO.output(27, 0)
+	time.sleep(0.003)    
+
     # Decrement capsule count
     coffee_capsule_dispenser[str(coffee_type)] = (coffee_capsule_dispenser[str(coffee_type)][0],  coffee_capsule_dispenser[str(coffee_type)][1] - 1)
     print "Coffee Machine: "+str(coffee_capsule_dispenser[str(coffee_type)][1])+" of "+str(coffee_type)+" remaining"    
@@ -100,3 +132,4 @@ def coffee_machine_control_server():
 
 if __name__ == "__main__":
     coffee_machine_control_server()
+
